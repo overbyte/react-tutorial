b279e735132f416d673c08264967dd6723de0885
Allandt Bik-Elliott <allandt@gmail.com>
2020-06-17 16:31:25 +0100

    add stepNumber to Game state and use this to allow user to jump to a step while maintaining the history until committed by starting a new turn from that step

--- src/index.js
+++ src/index.js
@@ -52,12 +52,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
-            isXNext: true
+            stepNumber: 0,
+            isXNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -71,14 +72,34 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             isXNext: !this.state.isXNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            isXNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={ () => this.jumpTo(move) }>{ desc }</button>
+                </li>
+            );
+        });
+
         let status;
 
         if (winner) {
@@ -97,7 +118,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{ status }</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{ moves }</ol>
                 </div>
             </div>
         );
